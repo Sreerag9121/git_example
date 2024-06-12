33ee563676876ea8a452eb02656e296d445289a6
Sreerag <sreerag9121@gmail.com>
2024-06-12 12:54:25 +0530

    Add app bar to Home page

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,6 +5,10 @@ class MyHomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+    );
   }
 }
